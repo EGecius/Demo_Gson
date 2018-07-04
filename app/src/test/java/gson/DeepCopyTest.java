9ed997542981055c59ff2bf7420b1d22623e516e
Egidijus Gecius <>
2018-07-04 11:54:04 +0100

    add 'in progress' demoHashMapDeepCopy

--- app/src/test/java/gson/DeepCopyTest.java
+++ app/src/test/java/gson/DeepCopyTest.java
@@ -12,12 +12,16 @@ import deepcopy.User;
 
 public class DeepCopyTest {
 
+    private static final String FIRST_NAME = "Prime";
+    private static final String LAST_NAME = "Minister";
+
+    Address address = new Address("Downing St 10", "London", "England");
+    User user = new User(FIRST_NAME, LAST_NAME, address);
+
     Gson gson = new Gson();
 
     @Test
     public void whenModifyingOriginalObject_thenGsonCloneShouldNotChange() {
-        Address address = new Address("Downing St 10", "London", "England");
-        User user = new User("Prime", "Minister", address);
 
         User userDeepCopy = gson.fromJson(gson.toJson(user), User.class);
 
@@ -26,5 +30,17 @@ public class DeepCopyTest {
         assertThat(userDeepCopy.getAddress().getCountry())
                 .isNotEqualTo(user.getAddress().getCountry());
     }
+
+//    @Test
+//    public void demoHashMapDeepCopy() {
+//
+//        HashMap<Integer, User> hashMap = new HashMap<>();
+//        hashMap.put(1, user);
+//
+//        HashMap<Integer, User> hashMapDeepCopy = gson.fromJson(gson.toJson(hashMap), HashMap.class);
+//
+//        User user = hashMapDeepCopy.get(1);
+//        assertThat(user.getFirstName()).isEqualTo(FIRST_NAME);
+//    }
 
 }
