9bd0c9aa4a6daa26dd2136dcddb12d07aa0db876
Egidijus Gecius <>
2018-07-04 12:35:38 +0100

    test reflection on MainActivity, to see if it complains with Gson

--- app/src/main/java/com/egecius/demo_gson/MainActivity.java
+++ app/src/main/java/com/egecius/demo_gson/MainActivity.java
@@ -1,7 +1,16 @@
 package com.egecius.demo_gson;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +19,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        doReflection();
+    }
+
+    private void doReflection() {
+        Log.v("Eg:MainActivity:25", "doReflection");
+
+        Gson gson = new Gson();
+        HashMap<Integer, String> hashMap = new HashMap<>();
+
+        hashMap.put(0, "zero");
+
+        String json = gson.toJson(hashMap);
+        Type type = new TypeToken<Map<Integer, String>>(){}.getType();
+        Map<String, String> mapDeepCopy = gson.fromJson(json, type);
+
+
+        Toast.makeText(this, mapDeepCopy.toString(), Toast.LENGTH_SHORT).show();
 
     }
 }
